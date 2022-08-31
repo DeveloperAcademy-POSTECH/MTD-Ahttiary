24beab92fee790a01899bb9376e613dcfc9e5519
Phainestha1 <250wons@gmail.com>
2022-08-31 17:26:54 +0900

    [Refactor] Update page number logic

--- Ahttiary/ViewModels/NoteManager.swift
+++ Ahttiary/ViewModels/NoteManager.swift
@@ -63,24 +63,7 @@ final class NoteManager: ObservableObject {
     }
     
     func fetchCurrentPage() -> String {
-        switch pageNumber {
-        case 0:
-            return "1"
-        case 1:
-            return "2"
-        case 2:
-            return "3"
-        case 3:
-            return "4"
-        case 4:
-            return "5"
-        case 5:
-            return "6"
-        case 6:
-            return "7"
-        default :
-            return "0"
-        }
+        return "\(pageNumber + 1)"
     }
 
 }
