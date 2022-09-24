5e5b23be5200a25fec84698b87c2e6abd238b409
seodam-hst <seodam.hst@gmail.com>
2022-09-24 16:41:27 +0900

    인지왜곡 선택 페이지와 두번째 감정 선택 페이지를 제거

--- Ahttiary/Views/Note/WriteNoteView.swift
+++ Ahttiary/Views/Note/WriteNoteView.swift
@@ -19,50 +19,39 @@ struct WriteNoteView: View {
         Group {
             switch noteManager.pageNumber {
             case 0:
+                // 상황 기록
                 WritePageView(
                     noteManager: noteManager,
+                    emotion: draftNote.firstEmotion,
                     answer: $draftNote.firstAnswer,
                     imageName: "noteAhtty"
                 )
             case 1:
-                WritePageView(
-                    noteManager: noteManager,
-                    answer: $draftNote.secondAnswer,
-                    imageName: "bbaeggomAhtty"
-                )
-            case 2:
+                // 감정 선택
                 SelectEmotionPageView(
                     noteManager: noteManager,
                     answer: $draftNote.firstEmotion,
                     imageName: "selectinAhtty"
                 )
-            case 3:
+            case 2:
+                // 긍정적 감정의 경우: 긍정적인 요소 확인
+                // 부정적 감정의 경우: 감정에 동반된 생각(자동적 사고) 기술
                 WritePageView(
                     noteManager: noteManager,
-                    answer: $draftNote.thirdAnswer,
+                    emotion: draftNote.firstEmotion,
+                    answer: $draftNote.secondAnswer,
                     imageName: "questionAhtty"
                 )
-            case 4:
-                SelectCognitiveDistortionPageView(
-                    noteManager: noteManager,
-                    answer: $draftNote.fourthAnswer,
-                    imageName: "helloAhtty"
-                )
-            case 5:
-                // 합리적 반응 도출
+            case 3:
+                // 긍정적 감정의 경우: 이를 유지하고 발전시키는 방법 탐색
+                // 부정적 감정의 경우: 이전 단계에서 기록한 생각을 객관적으로 평가
                 WritePageView(
                     noteManager: noteManager,
-                    answer: $draftNote.fifthAnswer,
-                    imageName: "thinkinAhtty"
+                    emotion: draftNote.firstEmotion,
+                    answer: $draftNote.thirdAnswer,
+                    imageName: "helloAhtty"
                 )
-            case 6:
-                // 두 번째 감정 체크
-                SelectEmotionPageView(
-                    noteManager: noteManager,
-                    answer: $draftNote.secondEmotion,
-                    imageName: "selectinAhtty"
-                )
-            case 7:
+            case 4:
                 // 마지막 페이지(내일도 즐거운 하루 보내자!)
                 EndPageView(
                     noteManager: noteManager,
