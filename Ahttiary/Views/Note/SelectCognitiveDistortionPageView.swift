8b6f35b12813f3516cfaee2d9dcedab7cf6735dd
seodam-hst <seodam.hst@gmail.com>
2022-10-01 20:49:02 +0900

    여러 변수를 따로 입력받는 대신, noteManager 하나만 입력받도록 변경했습니다. 이전, 다음 버튼을 누를 때 변경 사항이 저장되도록 했습니다.

--- Ahttiary/Views/Note/SelectCognitiveDistortionPageView.swift
+++ Ahttiary/Views/Note/SelectCognitiveDistortionPageView.swift
@@ -13,28 +13,31 @@ struct SelectCognitiveDistortionPageView: View {
     @ObservedObject var distortionPageManager: DistortionPageManager = DistortionPageManager()
     @EnvironmentObject var dateManager: DateViewModel
     
-    @Binding var answer: String
+    @State var answer: String
     var gridItemLayout = [GridItem(.flexible()), GridItem(.flexible())]
-    var draftNote: DraftNote
-    let imageName: String
+    
+    init(noteManager: NoteManager) {
+        self.noteManager = noteManager
+        self.answer = noteManager.getCurrentPageAnswer()
+    }
     
     var body: some View {
         ZStack {
             VStack {
                 CustomNavigationBar(
                     displayDate: dateManager.selectedDate,
-                    draftNote: draftNote
+                    draftNote: noteManager.draftNote
                 )
-                    .padding()
+                .padding()
                 
                 // 아띠와 말풍선
                 HStack(alignment: .center) {
-                    Image(imageName)
+                    Image(noteManager.getCurrentPageImageName())
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: ScreenSize.ahttyWriterWidth)
                     
-                    Text(noteManager.randomComments[noteManager.pageNumber])
+                    Text(noteManager.getCurrentPageComment())
                         .frame(
                             height: ScreenSize.questionMessageBoxHeight,
                             alignment: .center
@@ -58,19 +61,20 @@ struct SelectCognitiveDistortionPageView: View {
                     }// LazyVGrid
                 }// ScrollView
                 .padding(.horizontal)
-                                
-                Spacer()
                 
-                Text(noteManager.fetchCurrentPage() + "/7")
-                    .font(.custom(Font.Custom.comment, size: 20))
+                Spacer()
                 
                 // 화면 전환 버튼
                 HStack(spacing: 20) {
                     ChangePageButton("이전") {
+                        noteManager.updateDraftNote(answer: answer)
+                        Note.updateNote(using: noteManager.draftNote)
                         noteManager.goToPreviousPage()
                     }
                     
                     ChangePageButton("선택 완료") {
+                        noteManager.updateDraftNote(answer: answer)
+                        Note.updateNote(using: noteManager.draftNote)
                         noteManager.goToNextPage()
                     }
                     .disabled(answer.isEmpty)
@@ -79,7 +83,6 @@ struct SelectCognitiveDistortionPageView: View {
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color.Custom.background.ignoresSafeArea())
-            
             
             // 유형 설명 모달
             if distortionPageManager.isShowingDistortionCard {
